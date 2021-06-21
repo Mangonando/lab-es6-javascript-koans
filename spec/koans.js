117de4eb1dacf271987ba9229b3629750a1ff159
Mangonando <juan.f.ramirezv@gmail.com>
2021-06-21 15:17:17 +0200

    first one done

--- spec/koans.js
+++ spec/koans.js
@@ -8,9 +8,9 @@ describe('`let` restricts the scope of the variable to the current block - ', ()
   describe('`let` vs. `var`.', () => {
     it('`var` works as usual, it does not restricts scope', () => {
       if (true) {
-        /*You should add your code in here*/
+        var varX = true;
       }
-      // expect(varX).toBe(true);
+      expect(varX).toBe(true);
     });
 
     it('`let` restricts scope to inside the block', () => {
@@ -18,7 +18,7 @@ describe('`let` restricts the scope of the variable to the current block - ', ()
       if (true) {
         /*var or const? letX = true*/
       }
-      //expect(letX).toBe(false);
+      // expect(letX).toBe(false);
     });
 
     it('`var` does not restricts scope to inside the block in `for` loops', () => {
